abfcbeae8995799c3d46b4f08c9d8f82466b65dd
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2022-12-27 01:11:29 +0530

    What is Constructor and Self ? Comparing Objects.

--- WordGuessingGame.py
+++ WordGuessingGame.py
@@ -1,48 +1,34 @@
-# Overview
-
-# 1)Build a word guessing game, in which user guesses a word from list.
-# 2)Let’s say User selected a character if it's in the list it will move to next character to guess.
-# if its select all the character correct then the program will end.
-# 3)A random module will select random words from the list to guess for user.
-
-# requirement
-# random module
-
 import random
 
-Name = input("Enter your name:")
-print("Good luck", Name)
+name = input("Enter your name:")
+print("Good Luck",name)
 
-words = ['mercedes', 'ferrari', 'ford', 'maclaren', 'tata', 'suzuki', 'audi']
-word = random.choice(words)
+Sports = ['football','cricket','baseball','volleyball','badminton','chess','carrom','fighting']
+word = random.choice(Sports)
 
-print("Guess the  characters of car")
+print("Guess the sports name")
 guesses = ''
 
-turns = 7
+turns = 10
 while turns > 0:
-
     failed = 0
-
     for char in word:
         if char in guesses:
-            print(char, end=" ")
+            print(char,end=" ")
         else:
-            print("_")
-            failed += 1
+            print("___")
+            failed +=1
 
     if failed == 0:
-        print("You win", Name)
-        print("The word u guess which is", word)
-        break
+        print("congratulation",name)
 
-    print()
-    guess = input("Guess the character:")
+    guess = input("enter the character:")
     guesses += guess
 
-    if guess not in word:
-        turns -= 1
+    if  guess not in word:
+        turns -=1
         print("wrong")
-        print("You have", + turns, "left")
+        print(turns,"is left")
         if turns == 0:
-            print("you loose")
+            print("sorry try again")
+
